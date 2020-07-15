a65473479ef7d5d0f57e9d21611ef57fedd79e46
mllamzon <mllamzon@ucsd.edu>
2020-07-14 23:07:26 -0700

    Organized code

--- src/bankaccount/BankJFrame.java
+++ src/bankaccount/BankJFrame.java
@@ -7,36 +7,27 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-public class BankJFrame extends JFrame
-{
-    public BankJFrame(String title)
-    {
+public class BankJFrame extends JFrame {
+    public BankJFrame(String title) {
         super(title);
         FrameListener listener = new FrameListener();
         addWindowListener(listener);
     }
-    private class FrameListener extends WindowAdapter
-    {
-
-        public void windowClosing(WindowEvent e) 
-        {
-            if (BankAccount.savedAccount == false)
-            {
-            int option = JOptionPane.showConfirmDialog(null, "Would you like to save your accounts to a file?", "Save Account?", 
-                                                               JOptionPane.YES_NO_OPTION); // if user wants to save or not when transactions are added  
-            if(option == JOptionPane.YES_OPTION)
-            {
-                JFileChooser saveChooser = new JFileChooser();
-                int value = saveChooser.showSaveDialog(null);  
-                File file = saveChooser.getSelectedFile();
-                BankAccount.writeToFile(file);
+    private class FrameListener extends WindowAdapter {
+        public void windowClosing(WindowEvent e) {
+            if (BankAccount.savedAccount == false) {
+                int option = JOptionPane.showConfirmDialog(null, "Would you like to save your accounts to a file?", "Save Account?", JOptionPane.YES_NO_OPTION); 
+                if(option == JOptionPane.YES_OPTION) {
+                    JFileChooser saveChooser = new JFileChooser();
+                    int value = saveChooser.showSaveDialog(null);  
+                    File file = saveChooser.getSelectedFile();
+                    BankAccount.writeToFile(file);
+                }
+                System.exit(0);
             }
-            System.exit(0);
-            }
-            else
-            {
-            JOptionPane.showMessageDialog(null, "Now exiting program.");
-            System.exit(0);
+            else {
+                JOptionPane.showMessageDialog(null, "Now exiting program.");
+                System.exit(0);
             }
         }
     }   
